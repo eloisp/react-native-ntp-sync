85bf5cdadbde14efe0256fe5446a61a0842d1dce
Rafael Gomes <madrafael.gsilva@gmail.com>
2022-04-06 23:56:14 -0300

    Implements: time.windows.com

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ export default class NTPSync {
   public constructor({
     servers = [
       { server: "time.google.com", port: 123 },
+      { server: "time.windows.com", port: 123 },
       { server: "time.cloudflare.com", port: 123 },
       { server: "0.pool.ntp.org", port: 123 },
       { server: "1.pool.ntp.org", port: 123 },
