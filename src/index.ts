12bb65331dd24abd014de9614bdd2a5dedbb6b07
eloisp <eloisp@gmail.com>
2024-05-01 12:26:10 +0200

    Add getAvg

--- src/index.ts
+++ src/index.ts
@@ -4,179 +4,202 @@ import { NtpClientError } from "./error";
 import { getNetworkTime } from "./client";
 
 export default class NTPSync {
-  private ntpServers: Array<NtpServer>;
-  private limit: number;
-  private tickRate: number;
-  private syncTimeout: number;
-  private currentIndex = 0;
-  private tickId: number | null = null;
-  private historyDetails: NtpHistory;
-  private isOnline: boolean;
+    private ntpServers: Array<NtpServer>;
+    private limit: number;
+    private tickRate: number;
+    private syncTimeout: number;
+    private currentIndex = 0;
+    private tickId: number | null = null;
+    private historyDetails: NtpHistory;
+    private isOnline: boolean;
 
-  public constructor({
-    servers = [
-      { server: "time.google.com", port: 123 },
-      { server: "time.windows.com", port: 123 },
-      { server: "time.cloudflare.com", port: 123 },
-      { server: "0.pool.ntp.org", port: 123 },
-      { server: "1.pool.ntp.org", port: 123 },
-    ],
-    history = 10,
-    syncInterval = 300 * 1000,
-    syncTimeout = 10 * 1000,
-    syncOnCreation = true,
-    autoSync = true,
-    startOnline = true
-  }: Config = {}) {
-    this.ntpServers = servers;
-    this.limit = history;
-    this.tickRate = syncInterval;
-    this.syncTimeout = syncTimeout;
-    this.historyDetails = {
-      currentConsecutiveErrorCount: 0,
-      currentServer: this.ntpServers[this.currentIndex],
-      deltas: [],
-      errors: [],
-      isInErrorState: false,
-      lastSyncTime: null,
-      lastNtpTime: null,
-      lastError: null,
-      lifetimeErrorCount: 0,
-      maxConsecutiveErrorCount: 0,
-    };
-    this.isOnline = startOnline;
-
-    if (syncOnCreation && startOnline) {
-      this.syncTime();
-    }
-
-    if (autoSync) {
-      this.startAutoSync();
-    }
-  }
-
-  private computeAndUpdate = (ntpDate: Date): number => {
-    const tempServerTime = ntpDate.getTime();
-    const tempLocalTime = Date.now();
-    const dt = tempServerTime - tempLocalTime;
-    if (this.historyDetails.deltas.length === this.limit) {
-      this.historyDetails.deltas.shift();
-    }
-    this.historyDetails.deltas.push({
-      dt: dt,
-      ntp: tempServerTime,
-    });
-    this.historyDetails.lastSyncTime = tempLocalTime;
-    this.historyDetails.lastNtpTime = tempServerTime;
-    return dt;
-  };
-
-  public setIsOnline(isOnline: boolean) {
-    if (isOnline && !this.isOnline) {
-      this.isOnline = true;
-      this.syncTime();
-      this.startAutoSync();
-    } else if (!isOnline && this.isOnline) {
-      this.stopAutoSync();
-      this.isOnline = false;
-    }
-  }
-
-  public getIsOnline() {
-    return this.isOnline
-  }
-
-  public getDelta = async (): Promise<NtpDelta> => {
-    if (this.isOnline) {
-      const fetchingServer = Object.assign({}, this.historyDetails.currentServer);
-
-      try {
-        const ntpDate = await getNetworkTime(
-          this.historyDetails.currentServer.server,
-          this.historyDetails.currentServer.port,
-          this.syncTimeout
-        );
-        const delta = this.computeAndUpdate(ntpDate);
-
-        return {
-          delta,
-          fetchingServer,
+    public constructor({
+        servers = [
+            { server: "time.google.com", port: 123 },
+            { server: "time.windows.com", port: 123 },
+            { server: "time.cloudflare.com", port: 123 },
+            { server: "0.pool.ntp.org", port: 123 },
+            { server: "1.pool.ntp.org", port: 123 },
+        ],
+        history = 10,
+        syncInterval = 300 * 1000,
+        syncTimeout = 10 * 1000,
+        syncOnCreation = true,
+        autoSync = true,
+        startOnline = true
+    }: Config = {}) {
+        this.ntpServers = servers;
+        this.limit = history;
+        this.tickRate = syncInterval;
+        this.syncTimeout = syncTimeout;
+        this.historyDetails = {
+            currentConsecutiveErrorCount: 0,
+            currentServer: this.ntpServers[this.currentIndex],
+            deltas: [],
+            errors: [],
+            isInErrorState: false,
+            lastSyncTime: null,
+            lastNtpTime: null,
+            lastError: null,
+            lifetimeErrorCount: 0,
+            maxConsecutiveErrorCount: 0,
         };
-      } catch (err: any) {
-        this.shiftServer();
-        throw new NtpClientError(err, fetchingServer);
-      }
-    } else {
-      return { delta: 0 }
-    }
-  };
+        this.isOnline = startOnline;
 
-  public getHistory = (): NtpHistory => {
-    return JSON.parse(JSON.stringify(this.historyDetails)) as NtpHistory;
-  };
-
-  public getTime = () => {
-    let sum = this.historyDetails.deltas.reduce((a, b) => {
-      return a + b.dt;
-    }, 0);
-    let avg = Math.round(sum / this.historyDetails.deltas.length) || 0;
-    return Date.now() + avg;
-  };
-
-  private shiftServer = () => {
-    if (this.ntpServers.length > 1) {
-      this.currentIndex++;
-      this.currentIndex %= this.ntpServers.length;
-    }
-    this.historyDetails.currentServer = this.ntpServers[this.currentIndex];
-  };
-
-  public startAutoSync = () => {
-    if (!this.tickId) {
-      this.tickId = setInterval(() => this.syncTime(), this.tickRate);
-    }
-  };
-
-  public stopAutoSync = () => {
-    if (this.tickId) {
-      clearInterval(this.tickId);
-      this.tickId = null;
-    }
-  }
-
-  public syncTime = async (): Promise<boolean> => {
-    if (this.isOnline) {
-      try {
-        const delta = await this.getDelta();
-
-        this.historyDetails.currentConsecutiveErrorCount = 0;
-        this.historyDetails.isInErrorState = false;
-        return true;
-      } catch (err: any) {
-        var ed = {
-          name: err.name,
-          message: err.message,
-          server: err.server,
-          stack: err.stack,
-          time: Date.now(),
-        };
-        this.historyDetails.currentConsecutiveErrorCount++;
-        if (this.historyDetails.errors.length === this.limit) {
-          this.historyDetails.errors.shift();
+        if (syncOnCreation && startOnline) {
+            this.syncTime();
         }
-        this.historyDetails.errors.push(ed);
-        this.historyDetails.isInErrorState = true;
-        this.historyDetails.lastError = ed;
-        this.historyDetails.lifetimeErrorCount++;
-        this.historyDetails.maxConsecutiveErrorCount = Math.max(
-          this.historyDetails.maxConsecutiveErrorCount,
-          this.historyDetails.currentConsecutiveErrorCount
-        );
-      }
-      return false;
+
+        if (autoSync) {
+            this.startAutoSync();
+        }
     }
-    return false;
-  };
+
+    private computeAndUpdate = (ntpDate: Date): number => {
+        const tempServerTime = ntpDate.getTime();
+        const tempLocalTime = Date.now();
+        const dt = tempServerTime - tempLocalTime;
+        if (this.historyDetails.deltas.length === this.limit) {
+            this.historyDetails.deltas.shift();
+        }
+        this.historyDetails.deltas.push({
+            dt: dt,
+            ntp: tempServerTime,
+        });
+        this.historyDetails.lastSyncTime = tempLocalTime;
+        this.historyDetails.lastNtpTime = tempServerTime;
+        return dt;
+    };
+
+    public setIsOnline(isOnline: boolean) {
+        if (isOnline && !this.isOnline) {
+            this.isOnline = true;
+            this.syncTime();
+            this.startAutoSync();
+        } else if (!isOnline && this.isOnline) {
+            this.stopAutoSync();
+            this.isOnline = false;
+        }
+    }
+
+    public getIsOnline() {
+        return this.isOnline
+    }
+
+    public getDelta = async (): Promise<NtpDelta> => {
+        if (this.isOnline) {
+            const fetchingServer = Object.assign({}, this.historyDetails.currentServer);
+
+            try {
+                const ntpDate = await getNetworkTime(
+                    this.historyDetails.currentServer.server,
+                    this.historyDetails.currentServer.port,
+                    this.syncTimeout
+                );
+                const delta = this.computeAndUpdate(ntpDate);
+
+                return {
+                    delta,
+                    fetchingServer,
+                };
+            } catch (err: any) {
+                this.shiftServer();
+                throw new NtpClientError(err, fetchingServer);
+            }
+        } else {
+            return { delta: 0 }
+        }
+    };
+
+    public getHistory = (): NtpHistory => {
+        return JSON.parse(JSON.stringify(this.historyDetails)) as NtpHistory;
+    };
+
+    public getTime = () => {
+        let sum = this.historyDetails.deltas.reduce((a, b) => {
+            return a + b.dt;
+        }, 0);
+        let avg = Math.round(sum / this.historyDetails.deltas.length) || 0;
+        return Date.now() + avg;
+    };
+
+    public getAvg = () => {
+        let sum = this.historyDetails.deltas.reduce((a, b) => {
+            return a + b.dt;
+        }, 0);
+        let avg = Math.round(sum / this.historyDetails.deltas.length) || 0;
+
+        if (this.historyDetails.deltas.length >= 3) {
+            // Étape 2: Trouver le delta le plus éloigné de la moyenne
+            let deltaFarthestFromAvg = this.historyDetails.deltas.reduce((farthest, current) => {
+                return Math.abs(current.dt - avg) > Math.abs(farthest.dt - avg) ? current : farthest;
+            }, this.historyDetails.deltas[0]);
+
+            // Étape 3: Filtrer ce delta de la liste
+            let filteredDeltas = this.historyDetails.deltas.filter(delta => delta !== deltaFarthestFromAvg);
+
+            // Étape 4: Recalculer la moyenne sans le delta le plus éloigné
+            let newSum = filteredDeltas.reduce((a, b) => a + b.dt, 0);
+            avg = Math.round(newSum / filteredDeltas.length) || 0
+        }
+
+        return avg;
+    };
+
+    private shiftServer = () => {
+        if (this.ntpServers.length > 1) {
+            this.currentIndex++;
+            this.currentIndex %= this.ntpServers.length;
+        }
+        this.historyDetails.currentServer = this.ntpServers[this.currentIndex];
+    };
+
+    public startAutoSync = () => {
+        if (!this.tickId) {
+            this.tickId = setInterval(() => this.syncTime(), this.tickRate);
+        }
+    };
+
+    public stopAutoSync = () => {
+        if (this.tickId) {
+            clearInterval(this.tickId);
+            this.tickId = null;
+        }
+    }
+
+    public syncTime = async (): Promise<boolean> => {
+        if (this.isOnline) {
+            try {
+                const delta = await this.getDelta();
+
+                this.historyDetails.currentConsecutiveErrorCount = 0;
+                this.historyDetails.isInErrorState = false;
+                return true;
+            } catch (err: any) {
+                var ed = {
+                    name: err.name,
+                    message: err.message,
+                    server: err.server,
+                    stack: err.stack,
+                    time: Date.now(),
+                };
+                this.historyDetails.currentConsecutiveErrorCount++;
+                if (this.historyDetails.errors.length === this.limit) {
+                    this.historyDetails.errors.shift();
+                }
+                this.historyDetails.errors.push(ed);
+                this.historyDetails.isInErrorState = true;
+                this.historyDetails.lastError = ed;
+                this.historyDetails.lifetimeErrorCount++;
+                this.historyDetails.maxConsecutiveErrorCount = Math.max(
+                    this.historyDetails.maxConsecutiveErrorCount,
+                    this.historyDetails.currentConsecutiveErrorCount
+                );
+            }
+            return false;
+        }
+        return false;
+    };
 }
 
 export { Config, Delta, NtpDelta, NtpHistory, NtpServer, NtpClientError };
